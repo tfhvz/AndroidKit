227974fa4869d172534ae7822475412533594d0f
snice <520Tengai>
2015-11-15 14:54:48 +0800

    modify

--- androidkit/src/main/java/com/snicesoft/viewbind/base/IAv.java
+++ androidkit/src/main/java/com/snicesoft/viewbind/base/IAv.java
@@ -1,15 +1,8 @@
 package com.snicesoft.viewbind.base;
 
 interface IAv<H, D> {
-	public H newHolder();
 
-	public D newData();
+    public void dataBindAll();
 
-	public H getHolder();
-
-	public D getData();
-
-	public void dataBindAll();
-
-	public void dataBindTo(String fieldName);
+    public void dataBindTo(String fieldName);
 }

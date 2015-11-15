74415984a7af1fc60152a830a64e058fee97cf2d
snice <520Tengai>
2015-11-15 14:54:29 +0800

    modify

--- androidkit/src/main/java/com/snicesoft/viewbind/base/AvAppCompatActivity.java
+++ androidkit/src/main/java/com/snicesoft/viewbind/base/AvAppCompatActivity.java
@@ -6,10 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+import com.google.gson.internal.$Gson$Types;
 import com.snicesoft.viewbind.AVKit;
 import com.snicesoft.viewbind.ViewFinder;
+import com.snicesoft.viewbind.base.IAv;
+import com.snicesoft.viewbind.base.LayoutUtils;
 import com.snicesoft.viewbind.pluginmgr.Proxy;
 import com.snicesoft.viewbind.rule.IHolder;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 
 /**
  * FragmentActivity基类
@@ -24,16 +30,6 @@ public class AvAppCompatActivity<H extends IHolder, D> extends AppCompatActivity
     private ViewFinder finder;
     protected D _data;
     protected H _holder;
-
-    @Override
-    public final D getData() {
-        return _data;
-    }
-
-    @Override
-    public final H getHolder() {
-        return _holder;
-    }
 
     @Override
     public final void dataBindAll() {
@@ -64,14 +60,41 @@ public class AvAppCompatActivity<H extends IHolder, D> extends AppCompatActivity
         return clazz;
     }
 
-    @Override
-    public D newData() {
+    D newData() {
+        Class dClass = $Gson$Types.getRawType(getType(1));
+        if (dClass == Void.class)
+            return null;
+        try {
+            return (D) dClass.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
-    @Override
-    public H newHolder() {
+    H newHolder() {
+        Class hClass = $Gson$Types.getRawType(getType(0));
+        try {
+            if (hClass == Void.class)
+                return null;
+            return (H) hClass.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
         return null;
+    }
+
+    Type getType(int index) {
+        Type superclass = getThisClass().getGenericSuperclass();
+        if (superclass instanceof Class) {
+            throw new RuntimeException("Missing type parameter.");
+        }
+        ParameterizedType parameterized = (ParameterizedType) superclass;
+        return $Gson$Types.canonicalize(parameterized.getActualTypeArguments()[index]);
     }
 
     @Override

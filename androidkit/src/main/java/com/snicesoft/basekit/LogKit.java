102e0fcb4473c8e9fcf5b48f93bde2df64128574
zhuzhe <shaanxizhuzhe@163.com>
2015-11-19 14:21:09 +0800

    add

--- androidkit/src/main/java/com/snicesoft/basekit/LogKit.java
+++ androidkit/src/main/java/com/snicesoft/basekit/LogKit.java
@@ -18,11 +18,17 @@ package com.snicesoft.basekit;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Log工具，类似android.util.Log。
  * tag自动产生，格式: customTagPrefix:className.methodName(L:lineNumber),
  * customTagPrefix为空时只输出：className.methodName(L:lineNumber)。
- * <p>
+ * <p/>
  * Author: wyouflf
  * Date: 13-7-24
  * Time: 下午12:23
@@ -34,52 +40,23 @@ public class LogKit {
     private LogKit() {
     }
 
+    private static Gson gson = new Gson();
     public static boolean allowD = true;
     public static boolean allowE = true;
     public static boolean allowI = true;
     public static boolean allowV = true;
     public static boolean allowW = true;
     public static boolean allowWtf = true;
+    public static boolean allowJSON = true;
 
     private static String generateTag(StackTraceElement caller) {
-        String tag = "%s.%s(L:%d)";
+//        String tag = "%s.%s(L:%d)";
+        String tag = "%s.%s(" + caller.getFileName() + ":%d)";
         String callerClazzName = caller.getClassName();
         callerClazzName = callerClazzName.substring(callerClazzName.lastIndexOf(".") + 1);
         tag = String.format(tag, callerClazzName, caller.getMethodName(), caller.getLineNumber());
         tag = TextUtils.isEmpty(customTagPrefix) ? tag : customTagPrefix + ":" + tag;
         return tag;
-    }
-
-    public static CustomLogger customLogger;
-
-    public interface CustomLogger {
-        void d(String tag, String content);
-
-        void d(String tag, String content, Throwable tr);
-
-        void e(String tag, String content);
-
-        void e(String tag, String content, Throwable tr);
-
-        void i(String tag, String content);
-
-        void i(String tag, String content, Throwable tr);
-
-        void v(String tag, String content);
-
-        void v(String tag, String content, Throwable tr);
-
-        void w(String tag, String content);
-
-        void w(String tag, String content, Throwable tr);
-
-        void w(String tag, Throwable tr);
-
-        void wtf(String tag, String content);
-
-        void wtf(String tag, String content, Throwable tr);
-
-        void wtf(String tag, Throwable tr);
     }
 
     public static StackTraceElement getCallerStackTraceElement() {
@@ -90,169 +67,138 @@ public class LogKit {
         if (!allowD) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.d(tag, content);
-        } else {
-            Log.d(tag, content);
-        }
+        Log.d(tag, content);
     }
 
     public static void d(String content, Throwable tr) {
         if (!allowD) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.d(tag, content, tr);
-        } else {
-            Log.d(tag, content, tr);
-        }
+        Log.d(tag, content, tr);
     }
 
     public static void e(String content) {
         if (!allowE) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.e(tag, content);
-        } else {
-            Log.e(tag, content);
-        }
+        Log.e(tag, content);
     }
 
     public static void e(String content, Throwable tr) {
         if (!allowE) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.e(tag, content, tr);
-        } else {
-            Log.e(tag, content, tr);
-        }
+        Log.e(tag, content, tr);
     }
 
     public static void i(String content) {
         if (!allowI) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.i(tag, content);
-        } else {
-            Log.i(tag, content);
-        }
+        Log.i(tag, content);
     }
 
     public static void i(String content, Throwable tr) {
         if (!allowI) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.i(tag, content, tr);
-        } else {
-            Log.i(tag, content, tr);
-        }
+        Log.i(tag, content, tr);
     }
 
     public static void v(String content) {
         if (!allowV) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.v(tag, content);
-        } else {
-            Log.v(tag, content);
-        }
+        Log.v(tag, content);
     }
 
     public static void v(String content, Throwable tr) {
         if (!allowV) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.v(tag, content, tr);
-        } else {
-            Log.v(tag, content, tr);
-        }
+        Log.v(tag, content, tr);
     }
 
     public static void w(String content) {
         if (!allowW) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.w(tag, content);
-        } else {
-            Log.w(tag, content);
-        }
+        Log.w(tag, content);
     }
 
     public static void w(String content, Throwable tr) {
         if (!allowW) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.w(tag, content, tr);
-        } else {
-            Log.w(tag, content, tr);
-        }
+        Log.w(tag, content, tr);
     }
 
     public static void w(Throwable tr) {
         if (!allowW) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.w(tag, tr);
-        } else {
-            Log.w(tag, tr);
-        }
+        Log.w(tag, tr);
     }
 
+    public static <T> void json(T t) {
+        if (t == null)
+            return;
+        json(gson.toJson(t));
+    }
+
+    public static void json(String content) {
+        if (!allowJSON) return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+        printJSON(tag, content);
+    }
 
     public static void wtf(String content) {
         if (!allowWtf) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.wtf(tag, content);
-        } else {
-            Log.wtf(tag, content);
-        }
+        Log.wtf(tag, content);
     }
 
     public static void wtf(String content, Throwable tr) {
         if (!allowWtf) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.wtf(tag, content, tr);
-        } else {
-            Log.wtf(tag, content, tr);
-        }
+        Log.wtf(tag, content, tr);
     }
 
     public static void wtf(Throwable tr) {
         if (!allowWtf) return;
         StackTraceElement caller = getCallerStackTraceElement();
         String tag = generateTag(caller);
-
-        if (customLogger != null) {
-            customLogger.wtf(tag, tr);
-        } else {
-            Log.wtf(tag, tr);
-        }
+        Log.wtf(tag, tr);
     }
 
+    private final static String LINE_SEPARATOR = System.getProperty("line.separator");
+    private final static int JSON_INDENT = 4;
+    private final static String START = "╔═══════════════════════════════════════════════════════════════════════════════════════";
+    private final static String END = "╚═══════════════════════════════════════════════════════════════════════════════════════";
+
+    private static void printJSON(String tag, String msg) {
+        String message = "";
+        try {
+            if (msg.startsWith("{")) {
+                JSONObject jsonObject = new JSONObject(msg);
+                message = jsonObject.toString(JSON_INDENT);
+            } else if (msg.startsWith("[")) {
+                JSONArray jsonArray = new JSONArray(msg);
+                message = jsonArray.toString(JSON_INDENT);
+            } else {
+                message = msg;
+            }
+        } catch (JSONException e) {
+            message = msg;
+        }
+        Log.d(tag, START);
+        String[] lines = message.split(LINE_SEPARATOR);
+        for (String line : lines) {
+            Log.d(tag, "║ " + line);
+        }
+        Log.d(tag, END);
+    }
 }
--- app/src/main/java/com/snicesoft/androidkit/MainActivity.java
+++ app/src/main/java/com/snicesoft/androidkit/MainActivity.java
@@ -2,15 +2,21 @@ package com.snicesoft.androidkit;
 
 import android.os.Bundle;
 
+import com.snicesoft.androidkit.android.R;
 import com.snicesoft.basekit.LogKit;
 import com.snicesoft.framework.AKActivity;
 import com.snicesoft.net.api.API;
-import com.snicesoft.androidkit.android.R;
-
 import com.snicesoft.viewbind.annotation.Layout;
+import com.snicesoft.viewbind.rule.IHolder;
 
 @Layout(R.layout.activity_main)
-public class MainActivity extends AKActivity {
+public class MainActivity extends AKActivity<MainActivity.Holder, Void> {
+    public class Holder extends IHolder {
+        @Override
+        public void initViewParams() {
+            LogKit.d("===init");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

c208782c6d4bb66753263dc1ec9d87b59adee9ee
snice <520Tengai>
2016-03-20 13:33:53 +0800

    优化

--- androidkit/src/main/java/com/snicesoft/viewbind/base/AvAppCompatActivity.java
+++ androidkit/src/main/java/com/snicesoft/viewbind/base/AvAppCompatActivity.java
@@ -1,16 +1,12 @@
 package com.snicesoft.viewbind.base;
 
 import android.annotation.SuppressLint;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.gson.internal.$Gson$Types;
 import com.snicesoft.framework.AVFragment;
 import com.snicesoft.viewbind.AVKit;
@@ -32,11 +28,6 @@ import java.lang.reflect.Type;
 public abstract class AvAppCompatActivity<HD> extends AppCompatActivity implements IAv, OnClickListener {
     private ViewFinder finder;
     protected HD _hd;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     public ViewFinder getFinder() {
         return finder;
@@ -76,9 +67,6 @@ public abstract class AvAppCompatActivity<HD> extends AppCompatActivity implemen
         bindAll();
         onLoaded();
         loadNetData();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     protected final Fragment findFragmentById(int id) {
@@ -132,45 +120,5 @@ public abstract class AvAppCompatActivity<HD> extends AppCompatActivity implemen
         super.onDestroy();
         _hd = null;
         finder = null;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "AvAppCompat Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.snicesoft.viewbind.base/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "AvAppCompat Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.snicesoft.viewbind.base/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 }
